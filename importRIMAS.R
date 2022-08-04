be3b8eeb17f77eb3766b2296ac3d8e80c4f81494
Rubens Oliveira <rubensocj@gmail.com>
2022-08-04 11:28:40 -0300

    Update importRIMAS.R

--- importRIMAS.R
+++ importRIMAS.R
@@ -27,14 +27,13 @@
 import_RIMAS <- function(file) {
   require("dplyr")
   
-  # file <- file.gwl[1]
   # import as text
   x <- base::readLines(file, encoding = "latin1")
   x <- x[-which(x==";;;;;;")]
   x <- x[-which(x=="")]
   
   # header (n) e data (x)
-  n <- "N"#Ãºmero de sequencia do nÃ­vel d'agua;Data da mediÃ§Ã£o;Hora da mediÃ§Ã£o;NÃ­vel da Ã¡gua;VazÃ£o;Bombeando;"
+  n <- "N"
   for (i in 1:length(x)) {
     if (base::substr(x[i],1,1)==n) {
       kn <- i
@@ -64,10 +63,8 @@ import_RIMAS <- function(file) {
   dti <- xm$Data[1]
   dtf <- xm$Data[len]
   df <- data.frame(Data = seq(as.Date(dti), as.Date(dtf), by = 1))
-  # df <- suppressMessages(left_join(df, select(xm, Nivel), by = "Data"))
   df <- suppressMessages(df %>% dplyr::left_join(xm))
   df <- df %>% dplyr::select(Data, Nivel)
-  # names(df) <- c("Data", "Nivel")
   
   # day, month and year columns
   df <- df %>% dplyr::mutate(Ano = as.numeric(substr(Data, 1, 4)), .before = Nivel)
